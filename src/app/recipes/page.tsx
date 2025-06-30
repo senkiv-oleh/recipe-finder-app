569df832dc6bc764dd156f10816323c940d085da
Oleh Senkiv <o.senkiv.v@gmail.com>
2025-06-30 23:53:29 +0300

    Refactor RecipesPage to remove async handling and integrate RecipeList component for improved recipe display

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,40 +1,13 @@
-import { getRecipes } from '@/services/getRecipes';
-import { Recipe } from '@/types/Recipe';
-import RecipeCard from '@/components/RecipeCard';
-import { FetchedRecipe } from '@/types/FetchedRecipe';
+'use client';
 
-export default async function RecipesPage({
-  searchParams,
-}: {
-  searchParams: Promise<FetchedRecipe>;
-}) {
-  let recipes = [];
-    const resolvedSearchParams = await searchParams;
+import RecipeList from '@/components/RecipeList';
 
-
-  try {
-    recipes = await getRecipes({
-      query: resolvedSearchParams?.query || '',
-      cuisine: resolvedSearchParams?.cuisine
-        ? Array.isArray(resolvedSearchParams.cuisine)
-          ? resolvedSearchParams.cuisine
-          : [resolvedSearchParams.cuisine]
-        : undefined,
-      maxReadyTime: resolvedSearchParams?.maxReadyTime || '',
-    });
-  } catch (error) {
-    console.error('Failed to fetch recipes:', error);
-  }
-
-  if (!recipes || recipes.length === 0) {
-    return <p>No recipes found.</p>;
-  }
+export default function RecipesPage() {
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {recipes.map((recipe: Recipe) => (
-        <RecipeCard recipe={recipe} key={recipe.id} />
-      ))}
+    <div className="">
+      <RecipeList />
+    
     </div>
   );
 }
--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -3,16 +3,34 @@
 import { useRecipes } from '@/hooks/useRecipes';
 import RecipeCard from './RecipeCard';
 
-export default function RecipeList() {
+export default function RecipeList () {
   const { recipes, error, loading } = useRecipes();
-
+  
   if (loading) return <p>Loading...</p>;
+
+  if (!recipes || recipes.length === 0) {
+    return (
+      <div className='flex flex-col items-center justify-center h-full'>
+        <h2 className='text-2xl font-bold mb-4'>No Recipes Found</h2>
+        <p className='mb-4'>Try searching for a different recipe or cuisine.</p>
+        <p className='text-gray-500 mb-4'>It seems we couldn&#39;t find any recipes matching your search criteria.</p>
+        <p className='text-gray-500 mb-4'>You can try searching for different keywords or cuisines.</p>
+        <button
+          className='bg-blue-500 text-white px-4 py-2 rounded'
+          onClick={() => window.location.href='/'}
+        >
+          TRY AGAIN
+        </button>
+      </div>
+    );
+  }
+
 
   if (error) return <p>{error}</p>;
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-      {recipes.map((recipe) => (
+    <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+      {recipes.map(recipe => (
         <RecipeCard key={recipe.id} recipe={recipe} />
       ))}
     </div>
