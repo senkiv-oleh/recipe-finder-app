d2d56d5ab0906bded3038816cfae13e9d3a6721e
Oleh Senkiv <o.senkiv.v@gmail.com>
2025-07-02 21:55:26 +0300

    Refactor RecipeDetails component for improved layout and readability

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -15,36 +15,34 @@ const RecipeDetails = ({ recipe }: { recipe: RecipeDetailsType }) => {
         priority={false}
       />
 
+      <h1 className='text-3xl font-bold mb-4'>{recipe.title}</h1>
 
-        <h1 className='text-3xl font-bold mb-4'>{recipe.title}</h1>
-      
-          <div className='border-t-4 border-b-4 border-gray-300 pl-4 pr-4 py-4 mb-4'>
-          <p>
-            <strong className='font-semibold'>Cuisine:</strong>{' '}
-            {recipe.cuisines.join(', ')}
-          </p>
-          <p className='mt-2'>
-            <strong className='font-semibold'>Ready in:</strong>{' '}
-            {recipe.readyInMinutes} minutes
-          </p>
-          <p className=' mt-2'>
-            <strong className='font-semibold'>Servings:</strong>{' '}
-            {recipe.servings}
-          </p>
-          <p className='mt-2'>
-            <strong className='font-semibold'>Health Score:</strong>{' '}
-            {recipe.healthScore}
-          </p>
-        </div>
-        <div>
-          <h3 className='text-lg font-semibold mt-4'>Ingredients:</h3>
-          <ul className='list-disc list-inside'>
-            {recipe.extendedIngredients.map(ingredient => (
-              <li key={ingredient.id}>{ingredient.original}</li>
-            ))}
-          </ul>
-        </div>
-      <h3 className='text-lg font-semibold mt-4'>Summary:</h3>
+      <div className='border-t-4 border-b-4 border-gray-300 pl-4 pr-4 py-4 mb-4'>
+        <p>
+          <strong className='font-semibold'>Cuisine:</strong>{' '}
+          {recipe.cuisines.join(', ')}
+        </p>
+        <p>
+          <strong className='font-semibold'>Ready in:</strong>{' '}
+          {recipe.readyInMinutes} minutes
+        </p>
+        <p>
+          <strong className='font-semibold'>Servings:</strong> {recipe.servings}
+        </p>
+        <p>
+          <strong className='font-semibold'>Health Score:</strong>{' '}
+          {recipe.healthScore}
+        </p>
+      </div>
+      <div>
+        <h3 className='text-lg font-semibold mb-2'>Ingredients:</h3>
+        <ul className='list-disc list-inside'>
+          {recipe.extendedIngredients.map(ingredient => (
+            <li key={ingredient.id}>{ingredient.original}</li>
+          ))}
+        </ul>
+      </div>
+      <h3 className='text-lg font-semibold mb-2 mt-4'>Summary:</h3>
       <p dangerouslySetInnerHTML={{ __html: recipe.summary || '' }}></p>
     </div>
   );
